b0d43dc7ed96ada54814a109fef71f49af9d1b73
zZJoennZz <zzjoennzz@gmail.com>
2026-06-16 20:01:41 +0800

    Add lessons page with detailed class information and schedule; update index page with coach image

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
-// https://astro.build/config
 export default defineConfig({
     vite: {
-        plugins: [tailwindcss()]
-    }
+        plugins: [tailwindcss()],
+        server: {
+            watch: {
+                ignored: ['!**/src/**/*.astro'],
+            },
+        },
+    },
 });
